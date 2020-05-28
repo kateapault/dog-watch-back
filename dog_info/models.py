f7bad9bdc84f6197b5b069e3f8ea6ab4cdc653ce
github username <kateapault@gmail.com>
2020-05-28 17:22:43 -0400

    switched db to postgresql and remigrated

--- dog_info/models.py
+++ dog_info/models.py
@@ -6,25 +6,23 @@ from django.utils import timezone
 
 class Dog(models.Model):
     name = models.CharField(max_length=40)
-    birthday = models.CharField(max_length=10)
+    birthdate = models.DateField(blank=True, null=True)
     breed = models.CharField(max_length=40)
-    # picture = models.ImageField
-    
     
     def __str__(self):
         return self.name
     
     
-    # def age(self):
-    #     today = datetime.date.today()
-    #     age_in_days = today - self.birthday
-    #     age_in_years = math.floor(age_in_days / datetime.timedelta(days=365))
-    #     return age_in_years
-    #     return self.birthday
+    def age(self):
+        today = datetime.date.today()
+        age_in_days = today - self.birthday
+        age_in_years = math.floor(age_in_days / datetime.timedelta(days=365))
+        return age_in_years
+        return self.birthday
     
 class Food(models.Model):
-    when = models.DateTimeField
-    amount_value = models.FloatField
+    when = models.DateTimeField(blank=True, null=True)
+    amount_value = models.FloatField(blank=True, null=True)
     amount_measurement = models.CharField(max_length=15)
     dog = models.ForeignKey(
         Dog,
@@ -32,9 +30,9 @@ class Food(models.Model):
     )
 
 class Treat(models.Model):
-    when = models.DateTimeField
+    when = models.DateTimeField(blank=True, null=True)
     treat_type = models.CharField(max_length=25)
-    amount_value = models.FloatField
+    amount_value = models.FloatField(blank=True, null=True)
     amount_measurement = models.CharField(max_length=15)
     dog = models.ForeignKey(
         Dog,
@@ -42,15 +40,15 @@ class Treat(models.Model):
     )
 
 class Out(models.Model):
-    when = models.DateTimeField
+    when = models.DateTimeField(blank=True, null=True)
     dog = models.ForeignKey(
         Dog,
         on_delete=models.CASCADE
     )
     
 class Walkie(models.Model):
-    when = models.DateTimeField
-    distance_value = models.FloatField
+    when = models.DateTimeField(blank=True, null=True)
+    distance_value = models.FloatField(blank=True, null=True)
     distance_measurement = models.CharField(max_length=2)
     dog = models.ForeignKey(
         Dog,
